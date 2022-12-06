8ae9f99e98c63bfd3ac5d48afd39580b9abc0601
honghamin <113421639+honghamin@users.noreply.github.com>
2022-12-06 17:28:37 +0900

    13-01

--- main.c
+++ main.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct Book{
+struct Book
+{
 	int number;
 	char title[10];
 };
 
-int main(int argc, char *argv[]) {
-	int i;
-	struct Book ** bookshelf;
+void main(void) {
+	char*pc = NULL;
+	int i=0;
 	
-	bookshelf = (struct Bokk**)malloc(3*sizeof(struct Bokk*));
-	
-	for (i=0;i<3;i++)
-		bookshelf[i] = (struct Book*)malloc(10*sizeof(struct Book));
+	pc = (char*)malloc(100*sizeof(char));
+	if (pc == NULL){
+		printf("error!\n");
+		exit(1);}
 		
-	bookshelf[1][3].number = 5;
-	strcpy(bookshelf[1][3].title, "C ++ Programming");
-	
-	(bookshelf[2]+4)->number = 3;
-	strcpy((bookshelf[2]+4)->title, "Communication Theory");
-	
-	printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
-	printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
-	
-	for (i=0;i<3;i++)
-		free(bookshelf[i]);
-	free(bookshelf);
-	
-	return 0;
+	for (i=0;i<26;i++){
+		pc[i] = 'a'+i;
+	}
+	pc[i] = 0;
+	printf("%s\n", pc);
+	free(pc);
+   
+   return ;
 }
